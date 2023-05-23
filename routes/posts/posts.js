218cd8b3bbc9893d10b4e808ba49d0c59c7de1e2
Manipreeth <manipreethbolusani@gmail.com>
2023-05-24 01:44:56 +0530

    Created update,like and unlike post routes

--- routes/posts/posts.js
+++ routes/posts/posts.js
@@ -5,6 +5,9 @@ const {
   fetchPostsCtrl,
   fetchPostCtrl,
   deletePostCtrl,
+  updatepostCtrl,
+  likepostCtrl,
+  unlikepostCtrl,
 } = require("../../controllers/posts/posts");
 
 const multer = require("multer");
@@ -18,12 +21,23 @@ const upload = multer({ storage });
 postRouter.post("/", isLogin, upload.single("postImage"), createPostCtrl);
 
 //GET
-postRouter.get("/", fetchPostsCtrl);
+postRouter.get("/", isLogin, fetchPostsCtrl);
 
 //GET/:id
 postRouter.get("/:id", fetchPostCtrl);
 
 //DELETE/:id
 postRouter.delete("/:id", isLogin, deletePostCtrl);
+
+//PUT/:id
+postRouter.put("/:id", isLogin, upload.single("postImage"), updatepostCtrl);
+
+// Post/:id
+// route to like post
+postRouter.post("/like/:id", isLogin, likepostCtrl);
+
+// Post/:id
+// route to unlike post
+postRouter.post("/unlike/:id", isLogin, unlikepostCtrl);
 
 module.exports = postRouter;
