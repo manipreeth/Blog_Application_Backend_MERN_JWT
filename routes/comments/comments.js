979770ecbac6516bea2ab0ab8e819b3e1e27423b
Manipreeth <manipreethbolusani@gmail.com>
2023-05-24 01:51:58 +0530

    Created update and delete comment routes

--- routes/comments/comments.js
+++ routes/comments/comments.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const commentRouter = express.Router();
-const { createCommentCtrl } = require("../../controllers/comments/comments");
+const {
+  createCommentCtrl,
+  deleteCommentCtrl,
+  updateCommentCtrl,
+} = require("../../controllers/comments/comments");
+
 const isLogin = require("../../middlewares/isLogin");
 
 //POST
 commentRouter.post("/:id", isLogin, createCommentCtrl);
+
+//DELETE/:id
+commentRouter.delete("/:id", isLogin, deleteCommentCtrl);
+
+//PUT/:id
+commentRouter.put("/:id", isLogin, updateCommentCtrl);
 
 module.exports = commentRouter;
