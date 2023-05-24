0e7e5007e6d484e74d9cd19c299a3eb2d31a7564
Manipreeth <manipreethbolusani@gmail.com>
2023-05-24 17:07:09 +0530

    configured update and delete route

--- routes/comments/comments.js
+++ routes/comments/comments.js
@@ -2,8 +2,8 @@ const express = require("express");
 const commentRouter = express.Router();
 const {
   createCommentCtrl,
-  deleteCommentCtrl,
   updateCommentCtrl,
+  deleteCommentCtrl,
 } = require("../../controllers/comments/comments");
 
 const isLogin = require("../../middlewares/isLogin");
@@ -11,10 +11,10 @@ const isLogin = require("../../middlewares/isLogin");
 //POST
 commentRouter.post("/:id", isLogin, createCommentCtrl);
 
-//DELETE/:id
-commentRouter.delete("/:id", isLogin, deleteCommentCtrl);
-
 //PUT/:id
 commentRouter.put("/:id", isLogin, updateCommentCtrl);
+
+//DELETE/:id
+commentRouter.delete("/:id", isLogin, deleteCommentCtrl);
 
 module.exports = commentRouter;
